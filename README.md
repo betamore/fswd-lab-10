74f13cb5968ea72aee3eaa17f3eef5cf951817d1
David Raynes <David.Raynes@teamaol.com>
2016-10-12 17:55:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Full Stack Web Development -  Lab 10: Going Public
+
+## Setup
+
+* Fork and clone, as always
+* `npm install`
+* `npm run dev` (**not** `npm start`)
+
+## Tasks
+
+* Model hooks
+* Heroku account
---
+++ models/user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var bcrypt = require('bcrypt');
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        notIn: [['login']]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      set: function(val) {
+        this.setDataValue('password', bcrypt.hashSync(val, 8));
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+        notEmpty: true
+      }
+    }
+  }, {
+    instanceMethods: {
+      isValidPassword: function(password) {
+        return bcrypt.compareSync(password, this.password);
+      }
+    },
+    classMethods: {
+      associate: function(models) {
+        User.hasMany(models.Task);
+      }
+    }
+  });
+  return User;
+};
